ad5e8848429766420c8b1e37a1e71653e47e60af
cemir <cememir2017@gmail.com>
2023-02-10 22:13:32 +0300

    Eski kodlar eklendi

---
+++ Olds/1__types.py
@@ -0,0 +1,21 @@
+emir_yas = 9
+ıüğişöç = 123
+emir_isimleri = ('cem', 'emir')
+emir_soyisim = "yüksektepe"
+emir_boy = 132.5
+emir = {
+    'isim': 'Cem Emir',
+    'yas': 9,
+    'boy': 132.5,
+    'yillar': [2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
+}
+
+# print(type(emir_yas), emir_yas)
+# print(type(emir_isimleri), emir_isimleri)
+# print(type(emir_soyisim), emir_soyisim)
+# print(type(emir_boy), emir_boy)
+# print(type(emir), emir)
+
+print(emir['yillar'])  # bu satırda emir sözlüğündeki yillar değerini yazacak - 24.02.2022 21:10
+# print(ıüğişöç)
+print(emir_yas)
---
+++ Olds/2__functions.py
@@ -0,0 +1,25 @@
+def yaz():
+    print("yazdım")
+
+
+def topla(a, b):
+    print(a + b)
+
+
+def cikart(a, b):
+    return a - b
+
+
+def bol(a, b):
+    return a / b
+
+
+# 24.02.2022 21:32
+yaz()
+topla(8, 8)
+print(cikart(8, 8))
+print(bol(8, 8))
+topla("cem", "emir")
+topla("8", "8")
+# print(topla("8", 8))
+# print(topla(8, "8"))
