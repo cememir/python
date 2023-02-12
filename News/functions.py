924d54c9547a0ec0858aabaed667eaf3e8fc29a3
cememir <cememir2017@gmail.com>
2023-02-12 14:12:14 +0300

    def sinav_notu_ortalama(ilksinav, ikincisinav, hocanotu)

--- News/functions.py
+++ News/functions.py
@@ -1,12 +1,26 @@
-def emirgur():
-    print("def topla üst")
-    print(5+9)
-    print("def topla alt")
-def omer():
-    print("def omer üst")
-    print(5+9)
-    print("def omer alt")
+def sinav_notu_toplam(ilksinav, ikincisinav, hocanotu):
+    return ilksinav + ikincisinav + hocanotu
 
-print("üst omer()")
-#omer()
-print("alt omer()")
+
+def sinav_notu_ortalama(ilksinav, ikincisinav, hocanotu):
+    return (ilksinav + ikincisinav + hocanotu) // 3
+
+
+print(sinav_notu_toplam(hocanotu=58, ikincisinav=100, ilksinav=100))
+
+# print(sinav_notu_ortalama(58, 100, 100))
+
+# def emirgur():
+#     print("def topla üst")
+#     print(5+9)
+#     print("def topla alt")
+# def omer():
+#     print("def omer üst")
+#     print(5+9)
+#     print("def omer alt")
+#
+# print("üst omer()")
+# #omer()
+# print("alt omer()")
+#
+# emirgur()
