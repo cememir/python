fe097b76ff40667b2439b6f886d307de58a41f16
cememir <cememir2017@gmail.com>
2023-02-12 14:38:46 +0300

    # sep parametresi kullanılarak birden fazla veri yazdırılabilir

---
+++ News/print.py
@@ -0,0 +1,14 @@
+# sep parametresi kullanılarak birden fazla veri yazdırılabilir
+print("Merhaba", "Dünya!", sep="-")
+
+# sep parametresi değeri boşluk olmadığında veriler arasındaki ayracı belirleyebilirsiniz
+print("Merhaba", "Dünya!", sep=",")
+
+# sep parametresi belirtilmezse varsayılan olarak boşluk kullanılır
+print("Merhaba", "Dünya!")
+
+print("Merhaba", "Dünya!", "ben muslu", sep="sepsep", end=".\n")
+
+print("Merhaba", "Dünya!", "muslu", end=".\n\n\n\n")
+
+print("asd")
