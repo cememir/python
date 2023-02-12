c931c8212f93dc63ea841bc92857d7bf9a00e1cb
cememir <cememir2017@gmail.com>
2023-02-12 14:22:22 +0300

    while i < 10:

---
+++ News/while.py
@@ -0,0 +1,15 @@
+i = 0
+
+while i < 10:
+    print(f"i ==============> {i}")
+    i += 1
+    print(f"i += 1 {i}")
+
+
+
+
+
+
+
+# agalarscom = ["emiremir", "tugkan", "omer", "cememir"]
+# agalarscom = ("emiremir", "tugkan", "omer", "cememir")
