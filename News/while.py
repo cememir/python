13539234483da20d8e96165c1e2c7a3d83cd3044
cememir <cememir2017@gmail.com>
2023-02-12 14:31:32 +0300

    # while döngüsü ile listeyi yazdıralım

--- News/while.py
+++ News/while.py
@@ -1,14 +1,23 @@
-i = 0
+# bir liste oluşturalım
+numbers = [1, 2, 3, 4, 5, "q", "w", "a",82.5,35.7]
+# numbers = (1, 2, 3, 4, 5)
+# numbers.append(9)
 
-while i < 10:
-    print(f"i ==============> {i}")
-    i += 1
-    print(f"i += 1 {i}")
+liste_eleman_sayisi = len(numbers)
+print("liste_eleman_sayisi", liste_eleman_sayisi)
 
+print("numbers[-1]", numbers[-1])
+# while döngüsü ile listeyi yazdıralım
+k = 0
+while k < liste_eleman_sayisi:
+    print(numbers[k])
+    k += 1
 
-
-
-
+# i = 0
+# while i < 10:
+#     print(f"i ==============> {i}")
+#     i += 1
+#     print(f"i += 1 {i}")
 
 
 # agalarscom = ["emiremir", "tugkan", "omer", "cememir"]
