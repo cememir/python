658a64444e9fab77e41cfbe3cda13ffcffbdf652
cememir <cememir2017@gmail.com>
2023-02-12 14:02:08 +0300

    üst ömer alt ömer

---
+++ News/functions.py
@@ -0,0 +1,12 @@
+def emirgur():
+    print("def topla üst")
+    print(5+9)
+    print("def topla alt")
+def omer():
+    print("def omer üst")
+    print(5+9)
+    print("def omer alt")
+
+print("üst omer()")
+#omer()
+print("alt omer()")
