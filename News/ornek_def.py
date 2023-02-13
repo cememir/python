af68d8be511fb5b72dfc47a9b7bbbb175fa6af51
cememir <cememir2017@gmail.com>
2023-02-13 21:04:24 +0300

    örnek def dunya_saatleri

---
+++ News/ornek_def.py
@@ -0,0 +1,12 @@
+def dunya_saatleri(ulke):
+
+    if ulke == "turkiye":
+        print("türkiye saati")
+
+    if ulke == "rusya":
+        print("rusya saati")
+
+    if ulke == "abd":
+        print("abd saati")
+
+dunya_saatleri("abd")
