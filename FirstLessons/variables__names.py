ebccee4e451fe7a628983517e4c3fa0b8c8293e5
cemir <cememir2017@gmail.com>
2023-02-19 16:36:58 +0300

    print = "Cem Emir"

--- FirstLessons/variables__names.py
+++ FirstLessons/variables__names.py
@@ -1,3 +1,4 @@
+# 19.02.2023
 ad = "emir"
 AD = "EMİR"
 Ad = "Emir"
@@ -6,11 +7,18 @@ a_D = "a_D"
 a____________________D = "a____________________D"
 _ad = "_ad"
 _ad_1 = "_ad_1"
-üçağaçkılçığı = "üçağaçkılçığı" # non-ascii
-
-print(üçağaçkılçığı)
+# üçağaçkılçığı = "üçağaçkılçığı" # non-ascii
+# print(üçağaçkılçığı)
 # 1AD = 1
+
+# print = "Cem Emir" #str
+# print(print) # TypeError: 'str' object is not callable
+# "Cem Emir"("Cem Emir")
+
+
+
 exit()
+
 
 print("ad", ad)
 print("AD", AD)
