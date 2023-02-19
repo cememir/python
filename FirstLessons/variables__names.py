81b7ad1f22a5ab1f3f05ff67c6e036889358ec06
cemir <cememir2017@gmail.com>
2023-02-19 16:32:37 +0300

    değişken isimlerinin alabileceği değerler

--- FirstLessons/variables__names.py
+++ FirstLessons/variables__names.py
@@ -2,6 +2,15 @@ ad = "emir"
 AD = "EMİR"
 Ad = "Emir"
 aD = "emiR"
+a_D = "a_D"
+a____________________D = "a____________________D"
+_ad = "_ad"
+_ad_1 = "_ad_1"
+üçağaçkılçığı = "üçağaçkılçığı" # non-ascii
+
+print(üçağaçkılçığı)
+# 1AD = 1
+exit()
 
 print("ad", ad)
 print("AD", AD)
